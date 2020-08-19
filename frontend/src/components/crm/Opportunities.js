b44593a7584c67283342ce46bd31a074fb10e31c
Herve07h22 <herve.rincent@free.fr>
2020-08-19 15:37:34 +0200

    switched to Ant v4

--- frontend/src/components/crm/Opportunities.js
+++ frontend/src/components/crm/Opportunities.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import {Crud } from '../crud/Crud'
+import React from "react";
+import { Crud } from "../crud/Crud";
 
-const Opportunities = () => <Crud api="crm" ressource="opportunities" title="Opportunités d'affaires" 
-                            uploadCSV="Importer un fichier CSV" exportCSV="Exporter les données" 
-                            fields={[
-                                {name:"label", label:"Libellé", position : "primary", type : "string"},
-                                {name:"amount", label:"Montant", position : "primary", type : "float"},
-                                {name:"created_at", label:"Date de création", position : "primary", type : "date", readonly:true},
-                                {
-                                    name:"probability", 
-                                    label:"Probabilité", 
-                                    position : "primary", 
-                                    type :[
-                                        {id:"sure", value:"Certain"}, 
-                                        {id:"good", value:"Bonne"},
-                                        {id:"hard", value:"Incertain"},
-                                        {id:"testing", value:"Evaluation"},
-                                    ] 
-                                },
-                            ]}
-                            />
+const Opportunities = () => (
+  <Crud
+    api="crm"
+    ressource="opportunities"
+    title="Opportunités d'affaires"
+    fields={[
+      { name: "label", label: "Name", position: "primary", type: "string" },
+      { name: "amount", label: "Amount (€)", position: "primary", type: "float" },
+      { name: "created_at", label: "Created at", position: "primary", type: "date", readonly: true },
+      {
+        name: "probability",
+        label: "Probability",
+        position: "primary",
+        type: [
+          { id: "sure", value: "Sure" },
+          { id: "good", value: "Good" },
+          { id: "hard", value: "Hard" },
+          { id: "testing", value: "Test the market" },
+        ],
+      },
+    ]}
+  />
+);
 
-export default Opportunities
-
+export default Opportunities;
--- frontend/src/components/crud/CreateEdit.js
+++ frontend/src/components/crud/CreateEdit.js
@@ -1,95 +1,103 @@
-import React from 'react'
-import moment from 'moment'
-import {Form, InputNumber, Input, Button, message, Select, DatePicker } from 'antd'
-import {useJsqel} from '../../api/jsqel'
-
+import React from "react";
+import { Form, InputNumber, Input, Button, Select, DatePicker, Switch } from "antd";
+import { useJsqel } from "../../api/jsqel";
 const formItemLayout = {
-    labelCol: {
-        xs: { span: 24 },
-        sm: { span: 6 },
-    },
-    wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 12 },
-    },
-}
-
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 12 },
+  },
+};
 const tailFormItemLayout = {
-    wrapperCol: {
-        xs: {
-        span: 24,
-        offset: 0,
-        },
-        sm: {
-        span: 16,
-        offset: 6,
-        },
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
     },
-}
-
-const InputElement = ({type, readonly, api, ...rest}) => {
-    const [listItem, ]   = useJsqel(`${api}/list`, { sendItNow:(typeof type === "object"), table:type.ressource})
-    if (Array.isArray(type)) {
-        return (
-        <Select {...rest}>
-            { type.map(option => <Select.Option key={option.id} value={option.id}>{option.value}</Select.Option>)} 
-        </Select>
-        )
-    }
-
-    if (typeof type === "object") {
-        return (
-            <Select {...rest}>
-                { listItem && listItem.results && listItem.results.map(item => <Select.Option key={item.id} value={item.id}>{item[type.field]}</Select.Option>)} 
-            </Select>
-            )
-    }
-
-    switch(type) {
-        case 'date'     : return <DatePicker {...rest} />
-        case 'integer'  : return <InputNumber  {...rest} />
-        case 'password' : return <Input.Password  {...rest} />
-        default         : return <Input   {...rest} />
-    }
-}
-
-const FormElement = ({name, label, type, validation=[], currentValue, readonly, api, form}) => (
-    <Form.Item label={label} hasFeedback={!readonly}>
-        {form.getFieldDecorator(name, {
-        rules: validation.map( v => ({validator:v.rule, message:v.message})),
-        initialValue:type === "date" ? moment(currentValue) : currentValue
-        })(<InputElement type={type} disabled={readonly} api={api}/>)}
-    </Form.Item>
-)
-
-const CreateEdit = ({onSave,fields, currentItem, api, loading, form}) => {
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-        form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                onSave({...values, id:currentItem.id})
-            } else {
-                message.error(err)
-            }
-        })
-    }
-
+    sm: {
+      span: 16,
+      offset: 6,
+    },
+  },
+};
+const InputElement = ({ type, readonly, api, ...rest }) => {
+  const apiToUse = typeof type === "object" && type.api ? type.api : api;
+  const [listItem] = useJsqel(`${apiToUse}/list`, { sendItNow: typeof type === "object", table: type.ressource });
+  if (Array.isArray(type)) {
     return (
-        <Form {...formItemLayout} onSubmit={handleSubmit}> 
-            {fields.map(field => <FormElement key={field.name} api={api} form={form} currentValue={currentItem[field.name]} {...field} /> ) }
-            <Form.Item {...tailFormItemLayout}>
-                <Button type="primary" htmlType="submit" loading={loading}>
-                    Save
-                </Button>
-                &nbsp;
-                <Button type="secondary" onClick={()=>onSave(null)} >
-                    Cancel
-                </Button>
-            </Form.Item>
-        </Form>
-    )
-
-}
-
-export default Form.create()(CreateEdit);
+      <Select {...rest}>
+        {type.map((option) => (
+          <Select.Option key={option.id} value={option.id}>
+            {option.value}
+          </Select.Option>
+        ))}
+      </Select>
+    );
+  }
+  if (typeof type === "object") {
+    return (
+      <Select {...rest}>
+        {listItem &&
+          listItem.results &&
+          listItem.results.map((item) => (
+            <Select.Option key={item.id} value={item.id}>
+              {item[type.field]}
+            </Select.Option>
+          ))}
+      </Select>
+    );
+  }
+  switch (type) {
+    case "date":
+      return <DatePicker {...rest} />;
+    case "integer":
+      return <InputNumber {...rest} />;
+    case "password":
+      return <Input.Password {...rest} />;
+    case "boolean":
+      return <Switch {...rest} />;
+    default:
+      return <Input {...rest} />;
+  }
+};
+const FormElement = ({ name, label, type, validation = [], readonly, api, form }) => (
+  <Form.Item
+    label={label}
+    hasFeedback={!readonly && type !== "boolean"}
+    name={name}
+    rules={validation.map((v) => ({ validator: v.rule, message: v.message }))}
+    valuePropName={type === "boolean" ? "checked" : "value"}
+  >
+    <InputElement type={type} disabled={readonly} api={api} />
+  </Form.Item>
+);
+const CreateEdit = ({ onSave, fields, currentItem, api, loading, onValuesChange }) => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => onSave({ ...values, id: currentItem.id });
+  return (
+    <Form
+      {...formItemLayout}
+      onFinish={onFinish}
+      onValuesChange={(changedValues, allValues) => onValuesChange && onValuesChange(form, changedValues, allValues)}
+      initialValues={currentItem}
+      form={form}
+    >
+      {fields.map((field) => (
+        <FormElement key={field.name} api={api} form={form} {...field} />
+      ))}
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Save
+        </Button>
+        &nbsp;
+        <Button type="secondary" onClick={() => onSave(null)}>
+          Cancel
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default CreateEdit;
